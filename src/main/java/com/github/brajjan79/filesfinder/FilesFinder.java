2776256cb2c10a24414b8773ba7c89fd7b50f719
Anders B <brajjan79@gmail.com>
2021-03-18 09:29:18 +0100

    Add first set of code

---
+++ src/main/java/com/github/brajjan79/filesfinder/FilesFinder.java
@@ -0,0 +1,43 @@
+package com.github.brajjan79.filesfinder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.stringreformat.RegexFormat;
+
+public class FilesFinder {
+
+	/**
+	 * Find file/files based on the provided regex.
+	 * Traverses provided directory and subdirs.
+	 *
+	 * @param regex      - Regexp or search text
+	 * @param input_file - The provided file or directory
+	 * @return List of files, empty list if nothing is found.
+	 */
+	public static File[] find(String regex, File input_file) {
+		final List<File> fileList = findFiles(regex, input_file);
+		return fileList.toArray(new File[0]);
+	}
+
+	private static List<File> findFiles(String regex, File input_file) {
+		final List<File> fileList = new ArrayList<>();
+		if (input_file.isDirectory()) {
+			fileList.addAll(findFilesInDirectory(regex, input_file));
+		} else if (RegexFormat.hasMatch(input_file.getName(), regex)) {
+			fileList.add(input_file);
+		}
+		return fileList;
+	}
+
+	private static List<File> findFilesInDirectory(String regex, File input_file) {
+		final List<File> fileList = new ArrayList<>();
+		for (final File listed_files : input_file.listFiles()) {
+			fileList.addAll(findFiles(regex, listed_files));
+		}
+		return fileList;
+
+	}
+
+}
---
+++ src/test/java/com/github/brajjan79/FilesFinder/TestFilesFinder.java
@@ -0,0 +1,72 @@
+package com.github.brajjan79.FilesFinder;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
+
+import com.github.brajjan79.filesfinder.FilesFinder;
+
+public class TestFilesFinder {
+
+	private File mock_file;
+	private File mock_dir;
+	private File mock_dir_two;
+
+	@Before
+	public void setup() {
+		mock_file = PowerMockito.mock(File.class);
+		setupFile(mock_file, "mock_name.txt", false);
+		mock_dir = PowerMockito.mock(File.class);
+		setupFile(mock_dir, "mock_dir_name.txt", true);
+		mock_dir_two = PowerMockito.mock(File.class);
+		setupFile(mock_dir_two, "mock_dir_name_2.txt", true);
+	}
+
+
+	private void setupFile(File mock_entity, String name, boolean isDirectory) {
+		PowerMockito.when(mock_entity.isDirectory()).thenReturn(isDirectory);
+		PowerMockito.when(mock_entity.isFile()).thenReturn(!isDirectory);
+		PowerMockito.when(mock_entity.getName()).thenReturn(name);
+	}
+
+
+	@Test
+	public void testFindSingleFile() throws Throwable {
+		final File[] foundFiles = FilesFinder.find("mock_name", mock_file);
+		assertEquals("One matching files should have been found.", 1, foundFiles.length);
+	}
+
+	@Test
+	public void testFindSingleFileNoMatch() throws Throwable {
+		final File[] foundFiles = FilesFinder.find("no_match", mock_file);
+		assertEquals("No matching file should have been found.", 0, foundFiles.length);
+	}
+
+	@Test
+	public void testFindInSingleDirTwoFiles() throws Throwable {
+		PowerMockito.when(mock_dir.listFiles()).thenReturn(new File[]{mock_file, mock_file});
+		final File[] foundFiles = FilesFinder.find("mock_name", mock_dir);
+		assertEquals("Two matching files should have been found in dir.", 2, foundFiles.length);
+	}
+
+	@Test
+	public void testFindInTwoDirsFourFiles() throws Throwable {
+		PowerMockito.when(mock_dir.listFiles()).thenReturn(new File[]{mock_file, mock_file, mock_dir_two});
+		PowerMockito.when(mock_dir_two.listFiles()).thenReturn(new File[]{mock_file, mock_file});
+		final File[] foundFiles = FilesFinder.find("mock_name", mock_dir);
+		assertEquals("Four matching files should have been found in 2 dirs.", 4, foundFiles.length);
+	}
+
+	@Test
+	public void testFindTwoDirsNoFiles() throws Throwable {
+		PowerMockito.when(mock_dir.listFiles()).thenReturn(new File[]{mock_file, mock_file, mock_dir_two});
+		PowerMockito.when(mock_dir_two.listFiles()).thenReturn(new File[]{mock_file, mock_file});
+		final File[] foundFiles = FilesFinder.find("not_a_name_in_mock", mock_dir);
+		assertEquals("No matching files should have been found in 2 dirs.", 0, foundFiles.length);
+	}
+
+}
