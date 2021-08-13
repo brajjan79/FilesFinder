670d95eb08aae413d80d7346d9f968ddf5312d42
Anders B <brajjan79@gmail.com>
2021-08-13 15:29:28 +0200

    Add recursive option

--- src/main/java/com/github/brajjan79/filesfinder/FilesFinder.java
+++ src/main/java/com/github/brajjan79/filesfinder/FilesFinder.java
@@ -10,20 +10,34 @@ public class FilesFinder {
 
     /**
      * Find file/files based on the provided regex. Traverses provided directory and
-     * subdirs.
+     * sub directories. Will search recursively.
      *
      * @param regex      - Regexp or search text
      * @param input_file - The provided file or directory
      * @return List of files, empty list if nothing is found.
      */
     public static File[] find(String regex, File input_file) {
-        final List<File> fileList = findFiles(regex, input_file);
+        final List<File> fileList = findFiles(regex, input_file, false);
         return fileList.toArray(new File[0]);
     }
 
-    private static List<File> findFiles(String regex, File input_file) {
+    /**
+     * Find file/files based on the provided regex. Traverses provided directory and
+     * sub directories.
+     *
+     * @param regex      - Regexp or search text
+     * @param input_file - The provided file or directory
+     * @param recursive  - Search recursively or not
+     * @return List of files, empty list if nothing is found.
+     */
+    public static File[] find(String regex, File input_file, boolean recursive) {
+        final List<File> fileList = findFiles(regex, input_file, recursive);
+        return fileList.toArray(new File[0]);
+    }
+
+    private static List<File> findFiles(String regex, File input_file, boolean reqursive) {
         final List<File> fileList = new ArrayList<>();
-        if (input_file.isDirectory()) {
+        if (input_file.isDirectory() && reqursive) {
             fileList.addAll(findFilesInDirectory(regex, input_file));
         } else if (RegexFormat.hasMatch(input_file.getName(), regex)) {
             fileList.add(input_file);
@@ -34,10 +48,9 @@ public class FilesFinder {
     private static List<File> findFilesInDirectory(String regex, File input_file) {
         final List<File> fileList = new ArrayList<>();
         for (final File listed_files : input_file.listFiles()) {
-            fileList.addAll(findFiles(regex, listed_files));
+            fileList.addAll(findFiles(regex, listed_files, true));
         }
         return fileList;
-
     }
 
 }
